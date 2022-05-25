e7e579eb82fddae96f7c2ff331f09c6cabbc94dd
Mihnea <sanda.mihnea@yahoo.com>
2022-05-25 15:24:57 +0300

    Added a test for adding a police agent

---
+++ UnitTests/PoliceAgentsAddTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Police_Inspectorate.Controllers;
+using Police_Inspectorate.Models;
+using PoliceInspectorate.Context;
+
+
+namespace UnitTests
+{
+    public class PoliceAgentsAddTest
+    {
+        PoliceAgentsController policeAgentsController;
+        PoliceInspectorateContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+            policeAgentsController = new PoliceAgentsController(context);
+
+        }
+
+        [Test]
+        public void Create_Works()
+        {
+            //Arrange
+            var policeAgent = new PoliceAgent();
+
+            //Act
+            var addedPoliceAgent = policeAgentsController.Create(policeAgent);
+
+            //Assert
+            Assert.That(addedPoliceAgent, Is.Not.Null);
+
+        }
+    }
+}
