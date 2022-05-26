d2ed3e6b773bfb498cf5c62695e8d5425a8553ae
Mazureanu Grigorian <72690365+Grig13@users.noreply.github.com>
2022-05-26 14:10:55 +0300

    Added test for edit functionality for case controller.

---
+++ UnitTests/CaseEditTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Police_Inspectorate.Controllers;
+using Police_Inspectorate.Models;
+using PoliceInspectorate.Context;
+
+namespace UnitTests
+{
+    public class CaseEditTest
+    {
+        CasesController caseController;
+        PoliceInspectorateContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+            caseController = new CasesController(context);
+        }
+
+        [Test]
+        public void Edit_Works()
+        {
+            //Arrange
+            var cases = new Case();
+            var casesGuid = new Guid();
+
+            //Act
+            var editedCase = caseController.Edit(casesGuid, cases);
+
+            //Assert
+            Assert.That(editedCase, Is.Not.Null);
+
+        }
+    }
+}
